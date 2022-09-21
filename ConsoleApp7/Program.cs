21ec0a6e4888875b058693879872f27ac5e33a84
blich student <student-cyber@blich.co.il>
2022-09-21 15:31:55 +0300

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection.Metadata;
+class ConsoleApp2
+{
+    public static void Main()
+    {
+        int num;
+        Console.WriteLine("Enter two digits number: ");
+        num = int.Parse(Console.ReadLine());
+        if (num % 2 == 0)
+            num += 11;
+        else
+            num -= 11;
+        Console.WriteLine("The new number is {0}!", num);
+    }
+}
